fa4647bb86cf3fe6936b21d78fda59340b27e5c5
Reuven Ben Moshe <reuven2408@gmail.com>
2026-06-04 14:37:01 +0300

    Fix to mobile

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL
+const BASE_URL = import.meta.env.DEV && import.meta.env.VITE_API_URL
   ? `${import.meta.env.VITE_API_URL.replace(/\/+$/, '')}/api`
   : '/api'
 export const httpService = { get, post, put, del }
